c6e527eb3d5d9f5ae1e9382bcc33719af660f080
Lio Kaufman <lio.kaufman@gmail.com>
2017-04-25 16:15:27 +0200

    Add query current user reservations

---
+++ query/my_current_booking.php
@@ -0,0 +1,27 @@
+<?php
+// Report:  List all bookings and seats
+$query_current_user_reservations = "
+
+SELECT 
+    user.first_name,  
+    user.last_name,
+    booking.id,
+    date_format(date(booking.stamp),'%d-%M %Y'),
+    route.destination,
+    seat.num
+FROM `user`
+    INNER JOIN `payment` on user.id = payment.user_id
+    INNER JOIN `booking` on payment.id = booking.payment_id
+    INNER JOIN `reservation` on booking.id = reservation.booking_id
+    INNER JOIN `seat` on reservation.seat_id = seat.id
+    INNER JOIN `schedule` on booking.schedule_id = schedule.id
+    INNER JOIN `route` on schedule.route_id = route.id
+WHERE 
+    user.id =" . $_SESSION['user'] . "
+    AND date(schedule.departure) >= date_format(date(now()),'%y-%m-%d');
+"
+
+$res_current_user_reservations = mysqli_query($con, $query_current_user_reservations);
+$row_current_user_reservations = mysqli_fetch_array($res_current_user_reservations);
+$count_current_user_reservations = mysqli_num_rows($row_current_user_reservations);
+?>
